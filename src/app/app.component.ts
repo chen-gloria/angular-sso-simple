34c8e4c0a3b4b18e815bcbcb94f01fbfd42504e8
Gloria <chen.gloria@outlook.com>
2021-08-03 17:30:06 +1000

    A simple Angular sso app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './sso.config';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sso-app';
+
+  constructor(private oauthService:OAuthService){
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn(){
+    // this.oauthService.initCodeFlow();
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndLogin();
+  }
+
+  // login(){
+  //   this.oauthService.initImplicitFlow();
+  // }
+
+  // logout(){
+  //   this.oauthService.logOut();
+  // }
+
+  get token(){
+    let claims:any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
 }
